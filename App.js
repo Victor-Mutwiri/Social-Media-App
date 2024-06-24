e16a1ed9d88af206e3a036e7194ac315d380793c
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-06-24 21:51:33 +0300

    first app

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Title from './components/Title/Title';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Title title={'Lets explore'} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
